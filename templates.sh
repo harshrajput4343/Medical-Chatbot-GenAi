658a1c85a57a3ca308bed2f9ba678f8ad95487f1
harshrajput4343 <harshkumarsingh4343@gmail.com>
2025-08-06 17:26:57 +0530

    folder structure completed

---
+++ templates.sh
@@ -0,0 +1,17 @@
+mkdir -p src
+mkdir -p research
+
+
+#for creating files 
+touch src/__init__.py
+touch src/prompt.py
+touch src/helper.py
+
+touch app.py
+touch setup.py
+touch .env
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "files and repositories created successfully"
